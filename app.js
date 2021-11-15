e50fe58d2e3336ab3448eac0136fa2f7d40bb8f1
seth808rose <69048387+seth808rose@users.noreply.github.com>
2021-11-15 11:34:29 -0800

    test

--- app.js
+++ app.js
@@ -66,35 +66,4 @@ function getRandomHex() {
   return Math.floor(Math.random() * hex.length);
 }
 
-// ====================================
-//Number Counter
-// =========================================
 
-let count = 0;
-
-const btns = document.querySelectorAll(".btn");
-const value = document.querySelector(".count-value");
-
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    let style = e.currentTarget.classList;
-    if (style.contains("increase")) {
-      count++;
-    } else if (style.contains("decrease")) {
-      count--;
-    } else {
-      count = 0;
-    }
-    if(count > 0){
-      value.style.color = 'green'
-    }
-    if(count < 0){
-      value.style.color = 'red'
-    }
-    if(count == 0){
-      value.style.color = 'gray'
-    }
-
-    value.textContent = count;
-  });
-});
